6462aa1673cb32d384664070dfb7bfd37b05f5ff
Kevin <kevin.tarvin@gmail.com>
2017-01-02 22:44:49 -0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,9 @@
 
 # Check for updates and install Python3 with Nginx
 sudo apt-get update -y
-sudo apt-get install python3-pip python3-dev nginx -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-dev -y
+sudo apt-get install nginx -y
 
 
 #sudo apt-get install python-virtualenv
