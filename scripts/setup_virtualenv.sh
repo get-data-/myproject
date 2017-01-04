b960ab685af13d081465e52b8ab99fa538d89793
Kevin <kevin.tarvin@gmail.com>
2017-01-03 20:46:26 -0500

    create virtualenv in project folder

--- scripts/setup_virtualenv.sh
+++ scripts/setup_virtualenv.sh
@@ -4,6 +4,7 @@
 sudo pip3 install virtualenv
 
 # Create a Virtual Environment and install app reqs
+cd /home/ubuntu/myproject
 virtualenv myprojectenv
 source myprojectenv/bin/activate
 pip install -r /home/ubuntu/myproject/requirements.txt
