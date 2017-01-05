8aeace1ca7e39f2e5841caf2511b2a585025dcbc
Kevin <kevin.tarvin@gmail.com>
2017-01-05 13:37:48 -0500

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
+# Stop Nginx
 isExistApp=`pgrep nginx`
 if [[ -n  \$isExistApp ]]; then
    service nginx stop
 fi  
-
-# Remove Previous Build
-sudo rm -rf /home/ubuntu/myproject
-sudo rm /etc/systemd/system/myproject.service
-sudo rm /etc/nginx/sites-available/myproject
-sudo rm /etc/nginx/sites-enabled/myproject
